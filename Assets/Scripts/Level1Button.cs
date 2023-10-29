c55dd9cd9714aff3faee36947b0132545d84cda8
osirhcel <79244108+oSirhcel@users.noreply.github.com>
2023-10-30 00:45:19 +1100

    feat: Level 1 Button

---
+++ Assets/Scripts/Level1Button.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level1Button : MonoBehaviour
+{
+    public void LoadGameScene()
+    {
+
+        SceneManager.LoadScene("Scene");
+    }
+}
